64e33c5c25eab08a597b3104afa55f4d47a0bbca
Mitch Miller <socmodder@gmail.com>
2019-06-08 17:16:31 -0400

    add some more sounds

--- app/src/main/java/world/mitchmiller/bluessoundboard/MainActivity.kt
+++ app/src/main/java/world/mitchmiller/bluessoundboard/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         play_gloria_button.setOnClickListener(this)
         power_play_button.setOnClickListener(this)
         placeholder.setOnClickListener(this)
+        win_a_game_button.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -27,31 +28,49 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.stop_button -> stop()
             R.id.power_play_button -> powerPlay()
             R.id.placeholder -> buffaloHead()
+            R.id.win_a_game_button -> winAGame()
         }
+    }
+
+    private fun winAGame() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.lets_go_win_a_game)
+        setupPlayCompleteListener()
+        mediaPlayer.start()
+        stop_button.visibility = View.VISIBLE
     }
 
     private fun buffaloHead() {
         mediaPlayer = MediaPlayer.create(this, R.raw.buffalo_head)
+        setupPlayCompleteListener()
         mediaPlayer.start()
         stop_button.visibility = View.VISIBLE
     }
 
     private fun powerPlay() {
         mediaPlayer = MediaPlayer.create(this, R.raw.blues_power_play)
+        setupPlayCompleteListener()
         mediaPlayer.start()
         stop_button.visibility = View.VISIBLE
     }
 
     private fun playGloria() {
         mediaPlayer = MediaPlayer.create(this, R.raw.gloria)
+        setupPlayCompleteListener()
         mediaPlayer.start()
         stop_button.visibility = View.VISIBLE
     }
 
     private fun playGoalHorn() {
         mediaPlayer = MediaPlayer.create(this, R.raw.goal_horn)
+        setupPlayCompleteListener()
         mediaPlayer.start()
         stop_button.visibility = View.VISIBLE
+    }
+
+    private fun setupPlayCompleteListener() {
+        mediaPlayer.setOnCompletionListener {
+            stop_button.visibility = View.GONE
+        }
     }
 
     private fun stop() {
