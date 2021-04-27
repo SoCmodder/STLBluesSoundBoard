4bd98b0dd033e90a406e8368a6089074ee840b91
Mitch Miller <socmodder@gmail.com>
2021-04-27 17:44:36 -0400

    get list activity working

---
+++ app/src/main/java/world/mitchmiller/bluessoundboard/SoundClipListActivity.kt
@@ -0,0 +1,59 @@
+package world.mitchmiller.bluessoundboard
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import world.mitchmiller.bluessoundboard.adapter.SoundClipInfo
+import world.mitchmiller.bluessoundboard.adapter.SoundClipRecyclerAdapter
+import world.mitchmiller.bluessoundboard.databinding.ActivitySoundClipListBinding
+
+class SoundClipListActivity: AppCompatActivity(),
+    SoundClipRecyclerAdapter.OnSoundClipClickListener {
+
+    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var binding: ActivitySoundClipListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_sound_clip_list
+        )
+        setSupportActionBar(binding.toolbar)
+        setupRecyclerView()
+    }
+
+    private fun createSoundClipList(): ArrayList<SoundClipInfo> {
+        val soundClips: ArrayList<SoundClipInfo> = arrayListOf()
+        soundClips.add(SoundClipInfo("Power Play", R.raw.blues_power_play))
+        soundClips.add(SoundClipInfo("Buffalo Head", R.raw.buffalo_head))
+        soundClips.add(SoundClipInfo("GLORIA!", R.raw.gloria))
+        soundClips.add(SoundClipInfo("GOAL!", R.raw.goal_horn))
+        soundClips.add(SoundClipInfo("Lets Win A Game", R.raw.lets_go_win_a_game))
+        soundClips.add(SoundClipInfo("Turbulence", R.raw.turbulence))
+
+        return soundClips
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerview.layoutManager = LinearLayoutManager(this)
+
+        val adapter = SoundClipRecyclerAdapter(this, createSoundClipList(), this)
+        binding.recyclerview.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun playSoundClip(clipId: Int) {
+        mediaPlayer = MediaPlayer.create(this, clipId)
+        mediaPlayer?.setOnCompletionListener {
+
+        }
+        mediaPlayer?.start()
+        //TODO: Show Stop Button
+    }
+
+    override fun onSoundClipSelected(item: SoundClipInfo) {
+        playSoundClip(item.resourceId)
+    }
+}
--- app/src/main/java/world/mitchmiller/bluessoundboard/adapter/SoundClipRecyclerAdapter.kt
+++ app/src/main/java/world/mitchmiller/bluessoundboard/adapter/SoundClipRecyclerAdapter.kt
@@ -1,27 +1,47 @@
 package world.mitchmiller.bluessoundboard.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import world.mitchmiller.bluessoundboard.R
 
-class SoundClipRecyclerAdapter(val context: Context) : RecyclerView.Adapter<SoundClipRecyclerAdapter.SoundClipViewHolder>() {
-
-    private val clips : ArrayList<SoundClipInfo> = arrayListOf()
-
-    inner class SoundClipViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
+class SoundClipRecyclerAdapter(
+    private val context: Context,
+    private val clips: ArrayList<SoundClipInfo>,
+    private val listener: OnSoundClipClickListener
+) :
+    RecyclerView.Adapter<SoundClipRecyclerAdapter.SoundClipViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundClipViewHolder {
-        TODO("Not yet implemented")
+        val v = LayoutInflater.from(context).inflate(R.layout.sound_list_item, parent, false)
+        return SoundClipViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: SoundClipViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val soundClipInfo = clips[position]
+        holder.bind(soundClipInfo)
+        holder.itemView.setOnClickListener {
+            listener.onSoundClipSelected(soundClipInfo)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = clips.size
+
+    inner class SoundClipViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val title: TextView = itemView.findViewById(R.id.clip_title)
+        private val image: ImageView = itemView.findViewById(R.id.clip_image)
+
+        fun bind(info: SoundClipInfo) {
+            title.text = info.title
+        }
+    }
+
+    interface OnSoundClipClickListener {
+        fun onSoundClipSelected(item: SoundClipInfo)
     }
 }
